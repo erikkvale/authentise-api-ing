882f5342f4f336f88a1b6b5862d81a0a781e0acc
Erik Kvale <eirikval@gmail.com>
2018-08-06 11:27:07 -0500

    Added docstring

--- explore/utils/optimizations.py
+++ explore/utils/optimizations.py
@@ -2,7 +2,13 @@
 Some custom helpers for random optimizations
 """
 class lazyproperty:
-
+    """
+    A descriptor class to decorate read only class
+    attributes (properties) to be computed once on
+    access then store that computed value on the instance.
+    Saving multiple computations of the same property if
+    it is accessed multiple times. Thanks Beazley:)
+    """
     def __init__(self, func):
         self.func = func
 
