7c4a8c739480470c5552b3912762700e37188fd7
Erik Kvale <eirikval@gmail.com>
2018-08-10 14:46:30 -0500

    Added monolithic poc to get basic flask-restful, flask-sqlalchemy and updated .gitignore for build dir

--- build/app.py
+++ build/app.py
@@ -3,16 +3,31 @@ from flask_restful import Resource, Api
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:Gunnar14@localhost:5432/authentise'
 db = SQLAlchemy(app)
+api = Api(app)
 
 
-class UserHello(Resource):
+class User(db.Model):
+    __tablename__ = 'user'
+
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(100))
+    last_name = db.Column(db.String(100))
+    email = db.Column(db.String(200))
+    username = db.Column(db.String(200))
+    password = db.Column(db.String(128))
+    
+
+db.create_all()
+
+
+class UserResource(Resource):
 
     def get(self):
         return {'Hello': 'world'}
 
-api.add_resource(UserHello, '/')
+api.add_resource(UserResource, '/user')
 
 
 if __name__ == '__main__':
