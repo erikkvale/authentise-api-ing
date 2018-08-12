7ce2c8a5de0689325ecde864adf07be71eea10dc
Erik Kvale <eirikval@gmail.com>
2018-08-12 17:45:17 -0500

    Split out UserResource definition, tested endpoint with Postman, passed

--- build/app.py
+++ build/app.py
@@ -17,33 +17,13 @@ api = Api(app)
 
 from build.models import User
 from build.serializers import UserSchema
+from build.resources import UserResource
 
 # Initialize data tables
 db.drop_all()
 db.create_all()
 
-
-class UserResource(Resource):
-
-    def get(self):
-        users_schema = UserSchema(many=True)
-        users = User.query.all()
-        response = users_schema.dump(users)
-        return {'users': response}, 200
-
-    def post(self):
-        json_data = request.get_json()
-        username = User.query.filter_by(username=json_data['username']).first()
-        if username:
-            return {'message': 'username already exists!'}, 409
-        else:
-            user = User(**json_data)
-            db.session.add(user)
-            db.session.commit()
-            return {'message': 'user {} created!'.format(user.username)}, 201
-
-
-
+# API resource endpoints
 api.add_resource(UserResource, '/users')
 
 
