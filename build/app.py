aa3d1425beb025750135e07b20e7f633838eacfe
Erik Kvale <eirikval@gmail.com>
2018-08-13 07:54:55 -0500

    Unhooked config from vc

--- build/app.py
+++ build/app.py
@@ -5,14 +5,14 @@ from flask import Flask
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
-from build.config import DB_URI, DB_TRACK
+from build.config import DB_URI, DB_TRACK, JWT_SECRET_KEY
 
 
 # App configuration
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = DB_URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = DB_TRACK
-app.config['JWT_SECRET_KEY'] = 'derpyderp'
+app.config['JWT_SECRET_KEY'] = JWT_SECRET_KEY
 db = SQLAlchemy(app)
 api = Api(app)
 jwt = JWTManager(app)
