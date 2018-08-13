cbb265eed34c306d0e15b3acea72fc48b60e88be
Erik Kvale <eirikval@gmail.com>
2018-08-13 07:39:02 -0500

    Added Model serializer

--- build/serializers.py
+++ build/serializers.py
@@ -1,8 +1,20 @@
 from marshmallow import Schema, fields
 
+
 class UserSchema(Schema):
     first_name = fields.String()
     last_name = fields.String()
     email = fields.String()
     username = fields.String()
     password = fields.String()
+
+
+class ModelSchema(Schema):
+    name = fields.String()
+    surface_area = fields.Float()
+    volume = fields.Float()
+    size_x = fields.Float()
+    size_y = fields.Float()
+    size_z = fields.Float()
+    created_on = fields.DateTime()
+    user_id = fields.Integer()
